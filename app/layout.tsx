d1fc40c8347794e1fdf671e4fc3a5a20c408290b
Tiago Teixeira <ptteitia@carlsberggroup.com>
2024-08-27 09:25:15 +0100

    fix: openGraph image size

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,18 +5,20 @@ import "./globals.css";
 const font = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Hello!",
-  description: "I am Tiago, Frontend Developer",
+  title: "Hello! I am Tiago Teixeira, Frontend Developer!",
+  description:
+    "I'm a Web Frontend Developer located in Aveiro, Portugal. I have a passion for creating beautiful and functional web applications.",
   openGraph: {
-    title: "Hello!",
-    description: "I am Tiago, Frontend Developer",
+    title: "Hello! I am Tiago Teixeira, Frontend Developer!",
+    description:
+      "I'm a Web Frontend Developer located in Aveiro, Portugal. I have a passion for creating beautiful and functional web applications.",
     type: "website",
     siteName: "Tiago Teixeira",
     locale: "en_US",
     url: "https://www.me-tiagoteixeira.dev/",
     images: [
       {
-        url: "https://www.me-tiagoteixeira.dev/og.png",
+        url: "https://www.me-tiagoteixeira.dev/og-image.png",
         width: 1200,
         height: 630,
         alt: "Tiago Teixeira",
